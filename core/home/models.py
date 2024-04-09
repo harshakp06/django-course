a3ebc5c3e9737e6400acc039e5a441aba075a160
Harsha Vardhan <kpharsha@yahoo.com>
2024-04-09 14:57:52 +0530

    add

--- core/home/models.py
+++ core/home/models.py
@@ -11,6 +11,9 @@ class Student(models.Model):
     image = models.ImageField()
     file = models.FileField()
 
-class Product(models.Model):
-    pass
-    
+class Car(models.Model):
+    car_name = models.CharField(max_length=100)
+    speed = models.IntegerField(default=50)  
+
+    def __str__(self) -> str:
+        return self.car_name
