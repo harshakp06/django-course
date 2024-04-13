dabe1b6f4a859e9b4c770ef674a6cd8a96e48a4a
Harsha Vardhan <kpharsha@yahoo.com>
2024-04-13 21:33:47 +0530

    add

--- core/core/urls.py
+++ core/core/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from home.views import *
 from vege.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('',home, name="home"),
@@ -27,8 +30,17 @@ urlpatterns = [
     path('sucess_page/',sucess_page, name="sucess_page"),
     path('admin/', admin.site.urls),
 
+    path("delete_receipe/<id>/", delete_receipe, name="delete_receipe"),
+
     path('receipes/',receipes, name="receipes"),
 
     # path('html',html_page, name="html_page")
 ]
 
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
