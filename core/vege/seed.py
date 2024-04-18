7bce9b06f23e7f8eb3eca35f617e675012b874e8
Harsha Vardhan <kpharsha@yahoo.com>
2024-04-18 13:55:33 +0530

    add

--- core/vege/seed.py
+++ core/vege/seed.py
@@ -28,3 +28,19 @@ def seed_db(n=10)->None:
             student_age = student_age,
             student_address = student_address,
         )
+
+
+def create_subject_marks():
+    try:
+        student_objs = Student.objects.all()
+        for student in student_objs:
+            subjects = Subject.objects.all()
+            for subject in subjects:
+                SubjectMarks.objects.create(
+                    subject = subject,
+                    student =student,
+                    marks =random.randint(0,100)
+                )
+
+    except Exception as e:
+        print(e)
