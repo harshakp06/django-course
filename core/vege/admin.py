39a67be5374bf7505b9e9864e2562de08b61ed01
Harsha Vardhan <kpharsha@yahoo.com>
2024-04-19 21:41:15 +0530

    add report card

--- core/vege/admin.py
+++ core/vege/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import *
+from django.db.models import Sum
 
 admin.site.register(Receipe)
 
@@ -22,3 +23,17 @@ class SubjectMarkAdmin(admin.ModelAdmin):
     list_display = ['student','subject','marks']
 
 admin.site.register(SubjectMarks,SubjectMarkAdmin)
+
+class ReportCardAdmin(admin.ModelAdmin):
+    list_display = ['student','student_rank', 'total_marks','date_of_report_card_generation']
+    ordering = ['student_rank']
+    
+    def total_marks(self, obj):
+        subject_marks = SubjectMarks.objects.filter(student=obj.student)
+        marks=(subject_marks.aggregate(marks=Sum('marks')))
+        return marks["marks"]
+
+
+
+
+admin.site.register(ReportCard, ReportCardAdmin)
