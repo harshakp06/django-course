8fe022dfbc539a35a9b1ac7e58dbd55b5d5d3b45
Harsha Vardhan <kpharsha@yahoo.com>
2024-04-19 22:51:43 +0530

    add

---
+++ core/accounts/manager.py
@@ -0,0 +1,6 @@
+# from django.contrib.auth.base_user import BaseUserManager
+
+
+
+
+# class UserManager(BaseUserManager):
--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
+# # Create your models here.
+
+# from django.contrib.auth.models import AbstractUser
+
+
+# class CustomUser(AbstractUser):
+#     username = None
+#     phone_number = models.CharField(max_length=100, unique=True) 
+#     user_bio = models.CharField(max_length=50)
+#     email = models.EmailField(unique=True)
+#     user_profile_image = models.ImageField(upload_to="proile")
+
+#     USERNAME_FIELD = ['phone_number']
+#     REQUIRED_FIELDS = []
